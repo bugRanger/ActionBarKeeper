54ae81d85a6036136f97859abb34738cb820d377
bugRanger <departmentofjusticeevil@gmail.com>
2023-08-13 20:50:15 +0700

    Fix macro save

--- Macros.lua
+++ Macros.lua
@@ -9,24 +9,6 @@ local MAX_ACCOUNT_MACROS = 120
 local MAX_CHARACTER_MACROS = 18
 local MAX_TOTAL_MACROS = MAX_ACCOUNT_MACROS + MAX_CHARACTER_MACROS
 
-debug = function (str, ...)
-	if ... then str = str:format(...) end
-	DEFAULT_CHAT_FRAME:AddMessage(("ABK debug: %s"):format(str))
-end
-
-local function getMacroIndex(body)
-    local counter = 1
-    local len = string.len(body)
-    for i = 1, len, 3 do 
-      counter = math.fmod(counter*8161, 4294967279)
-        + (string.byte(body,i)*16776193)
-        + ((string.byte(body,i+1) or (len-i+256))*8372226)
-        + ((string.byte(body,i+2) or (len-i+256))*3932164)
-    end
-
-    return math.fmod(counter, 4294967291)
-end
-
 local function Compress(text)
 	text = string.gsub(text, "\n", "/n")
 	text = string.gsub(text, PARTS_SEPARATOR, PARTS_SEPARATOR_CODE)
@@ -61,7 +43,7 @@ function Macros:Save(type, macroIndex)
     body = Compress(body)
     texture = Compress(texture)
 
-    return { type, name, texture, body, isLocal }
+    return { type, name, texture, body, isLocal or 0 }
 end
 
 function Macros:Restore(actionId, ...)
@@ -77,13 +59,20 @@ function Macros:Restore(actionId, ...)
     body = Uncompress(body)
     texture = Uncompress(texture)
 
-    return nil
-    -- -- debug(string.format("pickup action: index - %s, macro - %s", actionId, name))
+    -- find macro in cache
+    -- if not find then createSpace() createMacro() end
+    -- pickup macro
+    -- place action
+    if not MacrosCache[name] then
+        debug(string.format("unknown macro: %s", name))
+        return nil
+    end
 
-    -- PickupMacro(self:FindMacro(actionId, name, content or -1))
-    -- PlaceAction(actionId)
-
-    -- debug(string.format("place action: index - %s", actionId))
+    local macroIndex = MacrosCache[name]
+    debug(string.format("pickup action: index - %s, macro - %s", actionId, name))
+    PickupMacro(macroIndex)
+    PlaceAction(actionId)
+    debug(string.format("place action: index - %s", actionId))
 end
 
 function Macros:Init()
@@ -94,9 +83,9 @@ function Macros:Init()
     local total = 0
 	for macroIndex = 1, MAX_TOTAL_MACROS do
 		local name, _, body, _ = GetMacroInfo(macroIndex)
-		if body then
+		if name and body then
             total = total + 1
-            MacrosCache[getMacroIndex(body)] = macroIndex
+            MacrosCache[name] = macroIndex
             debug(string.format("append macro: %s", name or "<empty>"))
         end
 	end
